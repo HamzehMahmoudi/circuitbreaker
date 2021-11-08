f94588570fe142241b76f9d43f2e80b1ca80d003
hamzeh mahmoodi <hamzehmahmoudi9@gmail.com>
2021-11-09 02:16:43 +0330

    count try and excepts

--- circuitbreaker.py
+++ circuitbreaker.py
@@ -6,16 +6,22 @@ def fallback(treshold,func, *args, **kwargs):
     """
     handle error here 
     """
+    ex = None
+    error_count = 0
+    try_count = 0
     for _ in range(treshold):
         try:
+            try_count += 1
             print(f"trying to call {func.__name__}")
             return func(*args, **kwargs)
         except Exception as e:
+            error_count += 1
+            ex = e 
             continue
     else:      
-        return {"status": "fail", "message": f"{type(e).__name__}"}
+        return {"status": "fail", "message": f"{type(ex).__name__}"}
 
-def circuitbreakear(fallback_function=fallback, treshold=3):
+def circuitbreakear(fallback_function=fallback, treshold=1):
     def wrapp(func):
         def wrapper(*args, **kwargs):
             try:
@@ -25,3 +31,9 @@ def circuitbreakear(fallback_function=fallback, treshold=3):
                 return fallback_function(treshold, func, *args, **kwargs)
         return wrapper
     return wrapp
+
+@circuitbreakear()
+def req():
+    requests.get("http://facebook.com")
+
+print(req())
