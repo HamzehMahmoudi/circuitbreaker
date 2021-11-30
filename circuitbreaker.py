fb7eed0cdaa0a3a4b69d7e3918fb32dc12a21ac8
hamzeh mahmoodi <hamzehmahmoudi9@gmail.com>
2021-11-30 14:21:58 +0330

    change basic structure

--- circuitbreaker.py
+++ circuitbreaker.py
@@ -1,38 +1,116 @@
-import logging
 import requests
-logger = logging.getLogger('django')
 
-def fallback(treshold,func, *args, **kwargs):
-    """
-    handle error here 
-    """
-    ex = None
-    error_count = 0
-    try_count = 0
-    for _ in range(treshold):
-        try:
-            try_count += 1
-            print(f"trying to call {func.__name__}")
-            return func(*args, **kwargs)
-        except Exception as e:
-            error_count += 1
-            ex = e 
-            continue
-    else:      
-        return {"status": "fail", "message": f"{type(ex).__name__}"}
+circuit_config=dict(treshold=3, timeout=3)
 
-def circuitbreakear(fallback_function=fallback, treshold=1):
+class CircuitBreaker:
+    """
+    CircuitBreaker class
+    """
+    STATE_OPEN = "open"
+    STATE_HALF_OPEN = "half_open"
+    STATE_CLOSE = "close"
+
+    circuits = []
+
+    def __init__(self, treshold=3, timeout=3):
+        self.treshold = treshold
+        self.timeout = timeout
+        self.state = CircuitBreaker.STATE_CLOSE
+        self.fail = 0
+        self.circuits.append(self)
+
+    def set_state(self):
+        if self.fail >= self.treshold:
+            self.open_circuit()
+        else:
+            self.close_circuit()
+    
+    def fallback(self , func, *args, **kwargs):
+        """
+        handle error here 
+        """
+        ex = None
+
+        if self.is_close():
+            for _ in range(self.treshold):
+                try:
+                    print(f"trying to call {func.__name__}")
+                    res = func(*args, **kwargs)
+                    return res
+                except Exception as e:
+                    self.fail += 1
+                    print(self.fail)
+                    ex = e
+                    continue
+            else:
+                self.open_circuit()
+                return {"status": "fail", "message": f"{type(ex).__name__}"}
+        elif self.is_open():
+            return {"status": "fail", "message": "we cant sent your request right now try again"}
+
+    def open_circuit(self):
+        self.state = CircuitBreaker.STATE_OPEN
+
+    def close_circuit(self):
+        """
+        close circuit breaker
+        """
+        self.state = CircuitBreaker.STATE_CLOSE
+
+    def half_open_circuit(self):
+        """
+        set state to half open
+        """
+        self.state = CircuitBreaker.STATE_HALF_OPEN
+
+    def is_open(self):
+        """
+        check if circuit is open
+        """
+        return self.state == CircuitBreaker.STATE_OPEN
+    
+    def is_close(self):
+        """
+        check if circuit is close
+        """
+        return self.state == CircuitBreaker.STATE_CLOSE
+
+    def close_all(self):
+        """
+        close all circuit
+        """
+        for circuit in self.circuits:
+            circuit.close_circuit()
+
+class CircuitMonitoring:
+    """
+    CircuitMonitoring class
+    """
+    def __init__(self, circuit_config):
+        self.circuit_config = circuit_config
+        self.circuit_breaker = CircuitBreaker(**circuit_config)
+
+    def monitor(self):
+        """
+        monitor circuit
+        """
+        self.circuit_breaker.state
+
+breaker = CircuitBreaker(**circuit_config)
+
+def circuit(cb=breaker , treshold=3, timeout=3):
     def wrapp(func):
+        
         def wrapper(*args, **kwargs):
             try:
                 return func(*args, **kwargs)
-            except Exception as e:
-                #logger.error(f"somthing wrong because {type(e).__name__} trying to handle it ")
-                return fallback_function(treshold, func, *args, **kwargs)
+            except:
+                return cb.fallback(func, *args, **kwargs)
         return wrapper
     return wrapp
 
-@circuitbreakear()
+
+@circuit()
 def req():
     requests.get("http://facebook.com")
 
